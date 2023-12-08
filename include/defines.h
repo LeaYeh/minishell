f0c6cd02116de4130ceeb29ec31644dcb928e2be
Lea Yeh <lyeh@c2r2p6.42vienna.com>
2023-12-08 21:33:38 +0100

    feat: Add promt color but has bug when delete

--- include/defines.h
+++ include/defines.h
@@ -6,16 +6,27 @@
 /*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 15:56:26 by lyeh              #+#    #+#             */
-/*   Updated: 2023/12/08 20:02:13 by lyeh             ###   ########.fr       */
+/*   Updated: 2023/12/08 21:32:56 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEFINES_H
 # define DEFINES_H
 
-# define EXIT_SUCCESS	0
-# define EXIT_FAILED	-1
+# define EXIT_SUCCESS		0
+# define EXIT_FAILED	    -1
 
-# define PROMPT			"minishell$ "
+// # define RED				"\033[0;31m"
+// # define GREEN				"\033[0;32m"
+// # define YELLOW				"\033[0;33m"
+// # define BLUE				"\033[0;34m"
+// # define MAGENTA			"\033[0;35m"
+// # define CYAN				"\033[0;36m"
+// # define WHITE				"\033[0;37m"
+// # define BOLD_RED			"\033[1;31m"
+// # define RESET				"\033[0m"
+
+# define PROMPT				"\001\033[1;32m\002minishell$ \001\033[0m\002"
+// # define PROMPT				"minishell$ "
 
 #endif	
