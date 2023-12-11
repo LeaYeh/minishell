ab95517afbccf24b485854ec738c2e488a03cc00
Lea Yeh <lyeh@c2r2p6.42vienna.com>
2023-12-11 16:23:56 +0100

    feat: Generate action table with different mode by args

--- data/gen_action_table.py
+++ data/gen_action_table.py
@@ -1,3 +1,6 @@
+import click
+
+
 ACTION_TYPES = {
     'accept': 0,
     'shift': 1,
@@ -6,77 +9,74 @@ ACTION_TYPES = {
 
 TOKEN_TYPES = ['WORD', 'ASSIGNMENT_WORD',
                 'RED_IN', 'RED_OUT',
-                'HEREDOC', 'APPEND',
+                'HERE_DOC', 'APPEND',
                 'PIPE', 'OR', 'AND',
-                'L_BRACKET', 'R_BRACKET']
+                'L_BRACKET', 'R_BRACKET', '$end']
 
 TOKEN_TYPES = {key: i for i, key in enumerate(TOKEN_TYPES)}
-
+import click
 RULE_NAMES = ['pipe_sequence', 'and_or', 'subshell',
               'command', 'redirect_list', 'simple_command',
               'cmd_name', 'cmd_word', 'cmd_prefix', 'cmd_suffix',
               'io_redirect', 'io_file', 'filename', 'io_here', 'here_end']
 RULE_NAMES = {key: i for i, key in enumerate(RULE_NAMES, 100)}
 
-def parse_state(state: str) -> int:
-    state_num = int(state.split()[1])
-    return state_num
 
-def get_token_type(token: str) -> int:
-    if token in TOKEN_TYPES.keys():
-        return TOKEN_TYPES[token]
-    elif token in RULE_NAMES.keys():
-        return RULE_NAMES[token]
-    return -1
+# Function to map a string to an int, with an optional verbose mode
+def map_string_to_int(s, mapping, verbose=False):
+    if verbose:
+        return s
+    return mapping.get(s, 'None' if verbose else -1)
 
-def get_action_type(action: str) -> int:
-    if action in ACTION_TYPES.keys():
-        return ACTION_TYPES[action]
-    return -1
 
-def get_next_state(action: str) -> int:
+def parse_state(state_str: str) -> int:
+    return int(state_str.split()[1])
+
+
+def get_token_type(token: str, verbose: bool = False) -> int:
+    return map_string_to_int(token, TOKEN_TYPES, verbose)
+
+
+def get_action_type(action: str, verbose: bool = False) -> int:
+    return map_string_to_int(action, ACTION_TYPES, verbose)
+
+
+def get_next_state(action: str, verbose: bool = False) -> int:
     if 'go to state' in action:
         return int(action.split()[-1])
-    return -1
-
-def parse_action(action):
-    action_term = action.split(", and go to state ")
-    if len(action_term) > 1:
-        # action_type = get_action_type(action_term[0].strip())
-        action_type = action_term[0].strip() if get_action_type(action_term[0].strip()) > -1 else -1
+    elif 'reduce using rule' in action:
+        tmp = action.split()[-1].strip('()')
+        return tmp if verbose else RULE_NAMES[tmp]
     else:
-        action_term = action.split()
-        # action_type = get_action_type(action_term[0].strip())
-        action_type = action_term[0].strip() if get_action_type(action_term[0].strip()) > -1 else -1
-    next_state = get_next_state(action)
-    return (action_type, next_state, -1)
-    #     action_state = int(action[1])
-    #     num_reduced_tokens = len(action[0].split())
-    # else:
-    #     action_state = None
-    #     num_reduced_tokens = 0
-    # return (action_type, action_state, num_reduced_tokens)
+        return 'None' if verbose else -1
 
-def parse_parsing_table(parsing_table):
+
+def parse_action(action, verbose: bool = False):
+    action_term = action.split(", and go to state ")
+    action_type = map_string_to_int(action_term[0].strip() if action_term else action.split()[0].strip(), ACTION_TYPES, verbose)
+    next_state = get_next_state(action, verbose)
+    return (action_type, next_state, -1)
+
+
+def parse_parsing_table(parsing_table: str, verbose: bool = False):
     lines = parsing_table.split("\n")
     action_table = {}
     current_state = None
     for line in lines:
-        if '11' in line:
-            print(line)
         if line.startswith("state"):
             current_state = parse_state(line)
             action_table[current_state] = {}
+        elif len(line.split()) and line.split()[0].isdigit():
+            pass
         elif line.strip().startswith("$default"):
             term = line.split()
-            action_table[current_state][-1] = (term[1], -1, -1)
-            # action_table[current_state][-1] = (get_action_type(term[1]), -1, -1)
+            action_table[current_state][-1] = (get_action_type(term[1], verbose), get_next_state(line, verbose), -1)
         elif line.strip() and current_state is not None:
             parts = line.split()
-            # token_type = get_token_type(parts[0])
-            token_type = parts[0] if get_token_type(parts[0]) > -1 else -1
-            action = parse_action(" ".join(parts[1:]))
-            if get_token_type(parts[0]) > -1:
+            token_type = map_string_to_int(parts[0], TOKEN_TYPES, verbose)
+            action = parse_action(" ".join(parts[1:]), verbose)
+            if (isinstance(token_type, str) and token_type != 'None') or \
+                (isinstance(token_type, int) and token_type > -1):
                 action_table[current_state][token_type] = action
     return action_table
 
@@ -88,8 +88,22 @@ def action_table_to_md(action_table):
             md_table += f"| {state} | {symbol} | {action_type} | {action_state if action_state is not None else ''} | {num_reduced_tokens} |\n"
     return md_table
 
-with open("data/parsing_rule.output", "r") as in_file:
-    with open("data/parsing_table.md", "w") as out_file:
-        action_table = parse_parsing_table(in_file.read())
-        md_table = action_table_to_md(action_table)
-        out_file.write(md_table)
+
+@click.command()
+@click.argument('input_file', type=click.Path(exists=True))
+@click.argument('output_file', type=click.Path())
+@click.option('-v', '--verbose', is_flag=True, help='Enable verbose mode')
+def main(input_file, output_file, verbose):
+    click.echo(f"Input file: {input_file}")
+    click.echo(f"Output file: {output_file}")
+    click.echo(f"Verbose mode: {'Enabled' if verbose else 'Disabled'}")
+
+    with open(input_file, "r") as in_file:
+        with open(output_file, "w") as out_file:
+            action_table = parse_parsing_table(in_file.read(), verbose)
+            md_table = action_table_to_md(action_table)
+            out_file.write(md_table)
+
+
+if __name__ == '__main__':
+    main()
