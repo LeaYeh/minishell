de964147c48fa40cc2e0c5d6fbbbfb146f9721ac
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2023-12-16 23:54:46 +0100

    feat: Add ft_lstinsert_after to libft
    The ft_lstinsert_after function inserts a new node after a given node in
    a singly linked list.

---
+++ libraries/libft/src/libft/lists/singly_linked/ft_lstinsert_after.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstinsert_after.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/24 16:04:12 by ldulling          #+#    #+#             */
+/*   Updated: 2023/12/16 23:49:45 by ldulling         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * The ft_lstinsert_after function inserts a new node after a given node in a
+ * singly linked list.
+ *
+ * @param lst   A double pointer to the node after which the new node should be
+ *              inserted.
+ *              If *lst is NULL, the new node is set as the first node.
+ * @param new   The new node to be inserted into the list.
+ */
+
+#include "libft.h"
+
+void	ft_lstinsert_after(t_list **lst, t_list *new)
+{
+	t_list	*last_new;
+	t_list	*old;
+
+	if (lst != NULL && new != NULL)
+	{
+		if (*lst == NULL)
+			*lst = new;
+		else
+		{
+			old = *lst->next;
+			*lst->next = new;
+			last_new = new;
+			while (last_new->next != NULL)
+				last_new = last_new->next;
+			last_new->next = old;
+		}
+	}
+	return ;
+}
