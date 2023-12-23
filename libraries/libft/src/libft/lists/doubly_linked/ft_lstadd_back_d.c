a295837ae18d0354b93d94ddaa656130c8a72ec2
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2023-12-23 15:19:48 +0100

    lib: Change ft_lstadd functions to add full lists to another list, not just a single node

--- libraries/libft/src/libft/lists/doubly_linked/ft_lstadd_back_d.c
+++ libraries/libft/src/libft/lists/doubly_linked/ft_lstadd_back_d.c
@@ -6,7 +6,7 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 16:04:12 by ldulling          #+#    #+#             */
-/*   Updated: 2023/12/20 19:08:49 by ldulling         ###   ########.fr       */
+/*   Updated: 2023/12/23 14:05:01 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,23 @@
 
 /**
  * Does not mark the new node as the end of the list by itself.
+ * Therefore, a whole list can be appended, not just one node.
  */
 void	ft_lstadd_back_d(t_list_d **lst, t_list_d *new)
 {
 	t_list_d	*cur;
 
-	if (lst != NULL && new != NULL)
+	if (lst == NULL || new == NULL)
+		return ;
+	if (*lst == NULL)
+		*lst = new;
+	else
 	{
-		if (*lst == NULL)
-			*lst = new;
-		else
-		{
-			cur = *lst;
-			while (cur->next != NULL)
-				cur = cur->next;
-			cur->next = new;
-			new->prev = cur;
-		}
+		cur = *lst;
+		while (cur->next != NULL)
+			cur = cur->next;
+		cur->next = new;
+		new->prev = cur;
 	}
 	return ;
 }
--- libraries/libft/src/libft/lists/singly_linked/ft_lstadd_back.c
+++ libraries/libft/src/libft/lists/singly_linked/ft_lstadd_back.c
@@ -6,31 +6,30 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 16:04:12 by ldulling          #+#    #+#             */
-/*   Updated: 2023/11/13 21:33:20 by ldulling         ###   ########.fr       */
+/*   Updated: 2023/12/23 14:29:36 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/**
- * Does not mark the new node as the end of the list by itself.
- */
-
 #include "libft.h"
 
+/**
+ * Does not mark the new node as the end of the list by itself.
+ * Therefore, a whole list can be appended, not just one node.
+ */
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*cur;
 
-	if (lst != NULL && new != NULL)
+	if (lst == NULL || new == NULL)
+		return ;
+	if (*lst == NULL)
+		*lst = new;
+	else
 	{
-		if (*lst == NULL)
-			*lst = new;
-		else
-		{
-			cur = *lst;
-			while (cur->next != NULL)
-				cur = cur->next;
-			cur->next = new;
-		}
+		cur = *lst;
+		while (cur->next != NULL)
+			cur = cur->next;
+		cur->next = new;
 	}
 	return ;
 }
