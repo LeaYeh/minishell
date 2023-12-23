d56ddb8f8b6f51f12a9bc974ac03ea2484b7faff
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2023-12-23 15:19:48 +0100

    lib: Change ft_lstadd functions to add full lists to another list, not just a single node

--- libraries/libft/src/libft/lists/singly_linked/ft_lstadd_front.c
+++ libraries/libft/src/libft/lists/singly_linked/ft_lstadd_front.c
@@ -6,23 +6,25 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 16:04:18 by ldulling          #+#    #+#             */
-/*   Updated: 2023/09/24 16:21:35 by ldulling         ###   ########.fr       */
+/*   Updated: 2023/12/23 11:32:51 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * Can prepend a whole list, not just one node.
+ */
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (lst != NULL && new != NULL)
+	if (lst == NULL || new == NULL)
+		return ;
+	if (*lst == NULL)
+		*lst = new;
+	else
 	{
-		if (*lst == NULL)
-			*lst = new;
-		else
-		{
-			new->next = *lst;
-			*lst = new;
-		}
+		ft_lstadd_back(&new, *lst);
+		*lst = new;
 	}
 	return ;
 }
