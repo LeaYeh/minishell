e0976aa6cf10073f667db0265be051adf80d01be
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2023-12-23 18:27:36 +0100

    doc: Add documentation to the rest of the libft functions that can segfault

--- libraries/libft/src/libft/strings/ft_strchr.c
+++ libraries/libft/src/libft/strings/ft_strchr.c
@@ -6,12 +6,26 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 16:13:12 by ldulling          #+#    #+#             */
-/*   Updated: 2023/09/24 16:14:51 by ldulling         ###   ########.fr       */
+/*   Updated: 2023/12/23 16:59:08 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * The ft_strchr function locates the first occurrence of c (converted to a
+ * char) in the string pointed to by s.
+ *
+ * @param s    The string in which to search for the character.
+ * @param c    The character to search for.
+ *
+ * @return     A pointer to the located character, or NULL if the character does
+ *             not appear in the string.
+ *
+ * @note       Calling ft_strchr with s equal to NULL will cause a segmentation
+ *             fault (mirrors the behavior of the original strchr).
+ *
+ */
 char	*ft_strchr(const char *s, int c)
 {
 	char	casted_c;
