b26337ca32d43f4bf3a75e2ace8d3ee93531e435
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2023-12-23 18:27:36 +0100

    doc: Add documentation to the rest of the libft functions that can segfault

--- libraries/libft/src/libft/strings/ft_strcmp.c
+++ libraries/libft/src/libft/strings/ft_strcmp.c
@@ -6,12 +6,29 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 16:16:22 by ldulling          #+#    #+#             */
-/*   Updated: 2023/11/12 11:18:04 by ldulling         ###   ########.fr       */
+/*   Updated: 2023/12/23 16:51:07 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * The ft_strcmp function compares the string pointed to by s1 with the string
+ * pointed to by s2.
+ *
+ * @param s1    A pointer to the first string.
+ * @param s2    A pointer to the second string.
+ *
+ * @return      0 if the strings are identical.
+ *              A positive integer if the first character that does not match
+ *              has a greater value in s1 than in s2.
+ *              A negative integer if the first character that does not match
+ *              has a lower value in s1 than in s2.
+ *
+ * @note        Calling ft_strcmp with s1 or s2 equal to NULL will cause a
+ *              segmentation fault (mirrors the behavior of the original
+ *              strcmp).
+ */
 int	ft_strcmp(const char *s1, const char *s2)
 {
 	const unsigned char	*casted_ptr_s1;
--- libraries/libft/src/libft/strings/ft_strlcpy.c
+++ libraries/libft/src/libft/strings/ft_strlcpy.c
@@ -6,14 +6,31 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 16:15:57 by ldulling          #+#    #+#             */
-/*   Updated: 2023/12/20 19:18:18 by ldulling         ###   ########.fr       */
+/*   Updated: 2023/12/23 17:15:07 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /**
- * @param size    Has to account for NUL-terminator.
+ * The ft_strlcpy function copies the NUL-terminated string from src to dst.
+ * It will copy at most size - 1 characters.
+ * It will then NUL-terminate, unless size is 0 or the src string is longer than
+ * size.
+ *
+ * @param dst     The string to copy to.
+ * @param src     The string to copy from.
+ * @param size    The total size of dst, including the space for the
+ *                NUL-terminator.
+ *
+ * @return        The total length of src.
+ *                If the return value is >= size, the output string has been
+ *                truncated.
+ *
+ * @note          Calling ft_strlcpy with dst equal to NULL with a size not 0,
+ *                or with src equal to NULL will cause a segmentation fault
+ *                (mirrors the behavior of the original strlcpy).
+ *
  */
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
