1d673fa5c66c483e7b2460cfd3fd920e56cf9b9a
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2023-12-23 11:18:56 +0100

    lib: Make ft_lst functions behave exactly the same as ft_stk functions
    Now the ft_lstclear and ft_lstdelone functions still free the node even when the function pointer argument is NULL. This really is more useful.
    
    And the ft_lstpop function now also sets the next ptr for the popped node to NULL.

--- libraries/libft/src/libft/lists/singly_linked/ft_lstclear.c
+++ libraries/libft/src/libft/lists/singly_linked/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 16:04:22 by ldulling          #+#    #+#             */
-/*   Updated: 2023/11/27 00:16:44 by ldulling         ###   ########.fr       */
+/*   Updated: 2023/12/23 11:17:18 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,13 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*cur;
 
-	if (lst != NULL && del != NULL)
+	if (lst == NULL)
+		return ;
+	while (*lst != NULL)
 	{
-		while (*lst != NULL)
-		{
-			cur = *lst;
-			*lst = (*lst)->next;
-			(*del)(cur->content);
-			free(cur);
-		}
+		cur = *lst;
+		*lst = (*lst)->next;
+		ft_lstdelone(cur, del);
 	}
 	return ;
 }
