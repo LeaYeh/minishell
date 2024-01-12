f708f4327fe0cffe818f7951b48773435e5cf5a5
Lea Yeh <lea.yeh.ml@gmail.com>
2024-01-12 22:01:35 +0000

    feat: Check error when execute command

--- source/backend/executor/exec_error.c
+++ source/backend/executor/exec_error.c
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cmd_error.c                                        :+:      :+:    :+:   */
+/*   exec_error.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/01 15:26:12 by lyeh              #+#    #+#             */
-/*   Updated: 2024/01/01 15:31:05 by lyeh             ###   ########.fr       */
+/*   Updated: 2024/01/11 20:54:14 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "defines.h"
 
-bool	check_cmd_name(char *cmd_name)
+bool	check_executable(char *filename)
 {
-	if (access(cmd_name, F_OK) != 0)
+	if (access(filename, F_OK) != 0)
 	{
-		ft_dprintf(2, "%s: %s:", PROGRAM_NAME, cmd_name);
-		perror("");
+		ft_dprintf(2, "%s: %s: command not found\n",
+			PROGRAM_NAME, filename);
 		return (false);
 	}
-	else if (access(cmd_name, X_OK) != 0)
+	else if (access(filename, X_OK) != 0)
 	{
-		ft_dprintf(2, "%s: %s: ", PROGRAM_NAME, cmd_name);
+		ft_dprintf(2, "%s: %s: ", PROGRAM_NAME, filename);
 		perror("");
 		return (false);
 	}
