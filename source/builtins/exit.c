87c9d362bf02680d1fdcb06f7edf5f9ce8c73181
Lukas <129603980+itislu@users.noreply.github.com>
2024-01-21 00:17:03 +0000

    refactor: Reduce duplicate code by combining handler functions in exit

--- source/builtins/exit.c
+++ source/builtins/exit.c
@@ -2,23 +2,18 @@
 #include "utils.h"
 #include "clean.h"
 
-void	handle_too_many_args(t_shell *shell, t_final_cmd_table *final_cmd_table)
+void	handle_exit(
+	t_shell *shell, t_final_cmd_table *final_cmd_table, int args_error)
 {
 	if (shell->subshell_level == 0)
 		printf("exit\n");
-	ft_dprintf(2, ERROR_EXIT_TOO_MANY_ARGS, PROGRAM_NAME, "exit");
-	if (shell->subshell_level != 0)
+	if (args_error == TOO_MANY_ARGS)
 	{
-		free_final_cmd_table(&final_cmd_table);
-		ft_clean_and_exit_shell(shell, shell->exit_code, NULL);
+		ft_dprintf(2, ERROR_EXIT_TOO_MANY_ARGS, PROGRAM_NAME, "exit");
+		if (shell->subshell_level == 0)
+			return ;
 	}
-}
-
-void	handle_general_exit(t_shell *shell, t_final_cmd_table *final_cmd_table)
-{
-	if (shell->subshell_level == 0)
-		printf("exit\n");
-	if (shell->exit_code == NOT_NUMERIC)
+	else if (args_error == NOT_NUMERIC)
 		ft_dprintf(2, ERROR_EXIT_NUMERIC_ARG,
 			PROGRAM_NAME, "exit", final_cmd_table->simple_cmd[1]);
 	free_final_cmd_table(&final_cmd_table);
@@ -38,9 +33,5 @@ void	exec_exit(t_shell *shell, t_final_cmd_table *final_cmd_table)
 		shell->exit_code = ((long long)ft_atof(args[0])) % 256;
 	else
 		shell->exit_code = args_error;
-	if (args_error != TOO_MANY_ARGS)
-		handle_general_exit(shell, final_cmd_table);
-	else
-		handle_too_many_args(shell, final_cmd_table);
-
+	handle_exit(shell, final_cmd_table, args_error);
 }
