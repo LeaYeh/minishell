eae95aa13a280ca01e817d3b7d63e3584e8e251c
LeaYeh <smile2140@gmail.com>
2024-01-30 22:44:38 +0100

    fix: Combine echo args into one string
    * printf without newline will not flash the buffer in time
        but ft_printf is not thread safe
    * Resolve issue #125

--- source/builtins/echo.c
+++ source/builtins/echo.c
@@ -8,12 +8,57 @@ bool	is_newline_option(char *str)
 	return (false);
 }
 
-// TODO: printf without newline will not flash the buffer in time
-// but ft_printf is not thread safe
+int	get_combined_args_len(char **args, bool end_with_newline)
+{
+	int		i;
+	int		len;
+
+	i = 0;
+	len = 0;
+	while (args[i])
+	{
+		len += ft_strlen(args[i]);
+		if (args[++i])
+			len++;
+	}
+	len += i;
+	if (end_with_newline)
+		len++;
+	return (len);
+}
+
+char	*combine_args(char **args, bool end_with_newline)
+{
+	int		i;
+	char	*str;
+	int		cur_len;
+
+	str = ft_calloc(
+			get_combined_args_len(args, end_with_newline), sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	cur_len = 0;
+	while (args[i])
+	{
+		if (i == 0)
+			ft_snprintf(str, ft_strlen(args[i]) + 1, "%s", args[i]);
+		else
+			ft_snprintf(
+				str, cur_len + ft_strlen(args[i]) + 1, "%s %s", str, args[i]);
+		cur_len += ft_strlen(args[i]);
+		i++;
+	}
+	if (end_with_newline)
+		ft_snprintf(str + cur_len, 2, "\n");
+	return (str);
+}
+
 int	ft_exec_echo(char **args)
 {
 	int		i;
 	bool	end_with_newline;
+	char	*combined_str;
 
 	i = 1;
 	if (is_newline_option(args[i]))
@@ -23,13 +68,10 @@ int	ft_exec_echo(char **args)
 	}
 	else
 		end_with_newline = true;
-	while (args[i])
-	{
-		ft_printf("%s", args[i]);
-		if (args[++i])
-			ft_printf(" ");
-	}
-	if (end_with_newline)
-		ft_printf("\n");
+	combined_str = combine_args(args + i, end_with_newline);
+	if (!combined_str)
+		return (MALLOC_ERROR);
+	ft_printf("%s", combined_str);
+	free(combined_str);
 	return (EXIT_SUCCESS);
 }
