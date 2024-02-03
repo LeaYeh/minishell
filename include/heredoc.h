6541a5841933808bf4bf720103d602d0f7132675
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-02-03 21:48:19 +0100

    fix: Fix line_list not being freed when Ctrl+D

--- include/heredoc.h
+++ include/heredoc.h
@@ -11,6 +11,6 @@ bool	setup_tmp_hdfile(int cmdtable_id, t_io_red *io_red);
 bool	expand_heredoc_content(t_shell *shell, t_list **line_list);
 bool	remove_here_end_quote(t_shell *shell,
 			t_io_red *io_red, bool *need_content_expansion);
-bool	append_line_to_list(char *line, t_list **line_list);
+bool	append_line_to_list(t_list **line_list, char *line);
 
 #endif
