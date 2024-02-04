cb3d2bde8441bb56fae76a16775fbcfa61830577
LeaYeh <smile2140@gmail.com>
2024-02-04 20:01:33 +0100

    rec: Fix error message as bash when bad substitution

--- include/heredoc.h
+++ include/heredoc.h
@@ -11,6 +11,7 @@ bool	setup_tmp_hdfile(int cmdtable_id, t_io_red *io_red);
 bool	expand_heredoc_content(t_shell *shell, t_list **line_list);
 bool	remove_here_end_quote(t_shell *shell,
 			t_io_red *io_red, bool *need_content_expansion);
+bool	write_content_to_file(char *content, char *filename);
 bool	append_line_to_list(t_list **line_list, char *line);
 
 #endif
