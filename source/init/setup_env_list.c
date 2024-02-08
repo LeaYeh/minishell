e229f72ba91717b5df914d6e64a6d4eb1f09ca5b
Lukas <129603980+itislu@users.noreply.github.com>
2024-02-08 03:04:37 +0100

    fix: Fix lost env vars with no value from shell init
    environ variables can never have a NULL value.
    Only variables with non-null value will be exported.
    That means, all variables in environ inherited from the previous process have at least an empty string as their value.

--- source/init/setup_env_list.c
+++ source/init/setup_env_list.c
@@ -6,22 +6,19 @@ static bool	extract_string(char **res, char *str, char *delim)
 	char	*tmp;
 
 	tmp = ft_strtok(str, delim);
-	if (tmp)
-	{
-		*res = ft_strdup(tmp);
-		if (str)
-			str[ft_strlen(str)] = '=';
-		if (!*res)
-			return (false);
-	}
-	else
-		*res = NULL;
+	if (!tmp)
+		tmp = "";
+	*res = ft_strdup(tmp);
+	if (str)
+		str[ft_strlen(str)] = '=';
+	if (!*res)
+		return (false);
 	return (true);
 }
 
 /*
  * PWD should always be set by current shell.
- * If OLDPWD exists and has value, if value is not a real directory,
+ * If OLDPWD exists and its value is not a real directory,
  * delete OLDPWD entirely (permissions don't matter).
  */
 bool	process_env_str_to_env_list(char *env_str, t_list **env_list)
@@ -39,7 +36,7 @@ bool	process_env_str_to_env_list(char *env_str, t_list **env_list)
 	}
 	else if (!extract_string(&value, NULL, ""))
 		return (free(key), false);
-	if (ft_strcmp(key, "OLDPWD") == 0 && value && !is_dir(value))
+	if (ft_strcmp(key, "OLDPWD") == 0 && !is_dir(value))
 	{
 		free(key);
 		free(value);
