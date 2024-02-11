3d9de0aed0ef28f9e8045ad294e20628b9702e3f
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-02-11 19:49:15 +0100

    lib: Add `ft_lstdup` to libft
    Duplicates a singly linked list.

--- libraries/libft/src/libft/lists/singly_linked/ft_lstdup.c
+++ libraries/libft/src/libft/lists/singly_linked/ft_lstdup.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/24 16:04:35 by ldulling          #+#    #+#             */
-/*   Updated: 2024/02/11 00:40:18 by ldulling         ###   ########.fr       */
+/*   Created: 2024/02/11 00:35:12 by ldulling          #+#    #+#             */
+/*   Updated: 2024/02/11 00:40:39 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstdup(t_list *lst, void *(*dup)(void *), void (*del)(void *))
 {
 	t_list	*cur;
 	t_list	*new_lst;
@@ -20,21 +20,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_node;
 	void	*new_content;
 
-	if (lst == NULL || f == NULL || del == NULL)
+	if (lst == NULL || dup == NULL || del == NULL)
 		return (NULL);
 	new_lst = NULL;
 	new_lst_tail = NULL;
 	cur = lst;
 	while (cur != NULL)
 	{
-		new_content = (*f)(cur->content);
+		new_content = (*dup)(cur->content);
+		if (new_content == NULL)
+			return (ft_lstclear(&new_lst, del), NULL);
 		new_node = ft_lstnew(new_content);
 		if (new_node == NULL)
-		{
-			del(new_content);
-			ft_lstclear(&new_lst, del);
-			return (NULL);
-		}
+			return (del(new_content), ft_lstclear(&new_lst, del), NULL);
 		ft_lstadd_back_eff(&new_lst, &new_lst_tail, new_node);
 		cur = cur->next;
 	}
