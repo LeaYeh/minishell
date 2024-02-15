4c76304d75d2405d001262669875198cf31846d6
Lukas <129603980+itislu@users.noreply.github.com>
2024-02-15 10:53:27 +0100

    style: Rename to noun filenames in expander

--- include/expander.h
+++ include/expander.h
@@ -18,28 +18,13 @@
 /* bad_substitution.c */
 bool	is_bad_substitution(char *str, t_expander_op op_mask);
 
-/* expander_task_stack.c */
-bool	create_expander_task_stack(t_list **task_stack, char *new_str, \
-									t_expander_op op_mask);
-
-/* expand_special_param.c */
-bool	expand_exit_code(char **new_str, t_expander_task *task, int exit_code);
-
-/* expand_variable.c */
-bool	expand_variable(char **new_str, t_expander_task *task, \
-						t_list *env_list);
-size_t	get_varname_len(char *str);
-
-/* expand.c */
-bool	expand(char **new_str, t_list **lst, t_shell *shell, \
-				t_expander_op op_mask);
-bool	execute_expander_task_stack(char **new_str, t_list *task_stack, \
-									t_list **lst, t_shell *shell);
-bool	is_null_expansion(char *dup, t_list *task_stack);
-
 /* expander.c */
 int		ft_expander(char *str, t_list **lst, t_shell *shell, \
 					t_expander_op op_mask);
+
+/* expander_task_stack.c */
+bool	create_expander_task_stack(t_list **task_stack, char *new_str, \
+									t_expander_op op_mask);
 
 /* expander_utils.c */
 size_t	get_offset(char *str);
@@ -48,7 +33,22 @@ bool	is_open_pair(unsigned char c, t_is_open_pair_op operation);
 void	skip_to_dollar_not_in_single_quotes(char *str, size_t *i);
 void	skip_to_expander_symbol(char *str, size_t *i);
 
-/* remove_quotes.c */
+/* expansion_handler.c */
+bool	expand(char **new_str, t_list **lst, t_shell *shell, \
+				t_expander_op op_mask);
+bool	execute_expander_task_stack(char **new_str, t_list *task_stack, \
+									t_list **lst, t_shell *shell);
+bool	is_null_expansion(char *dup, t_list *task_stack);
+
+/* quote_removal.c */
 bool	remove_quote(char **new_str, t_expander_task *task);
+
+/* special_param_expansion.c */
+bool	expand_exit_code(char **new_str, t_expander_task *task, int exit_code);
+
+/* variable_expansion.c */
+bool	expand_variable(char **new_str, t_expander_task *task, \
+						t_list *env_list);
+size_t	get_varname_len(char *str);
 
 #endif
