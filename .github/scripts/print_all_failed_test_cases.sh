9243c07e0b97e24ed1574ffa76c21a707337f821
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-02-27 15:01:49 +0100

    ci: Print result block in the regression test

--- .github/scripts/print_all_failed_test_cases.sh
+++ .github/scripts/print_all_failed_test_cases.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TEST_OUTPUT=$(cat "$RESULT_FILE")
+RESULT=0
 
 # Extract line numbers and file paths from test output
 while IFS= read -r line; do
@@ -15,6 +16,15 @@ while IFS= read -r line; do
     echo -e "\e[91m---------------------------------------------------------------------------------\e[1;97m"
     sed -n "${line_number},\$p" "$file_path" | awk 'NF {p=1} !NF {if(p) exit} {if(p) print}'
     echo -e "\e[0;91m=================================================================================\e[0m"
+  fi
+
+  # Check for the start of the result block and set the flag
+  if [[ $line =~ ^\s*[🏁]+\s*$ ]]; then
+    RESULT=1
+  fi
+  # If the flag is set, print the line
+  if [[ $RESULT -ne 0 ]]; then
+    echo "$line"
   fi
 
   line_number=""
