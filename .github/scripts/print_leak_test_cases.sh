e2167036dee543d69e5c9571c4477709c0c0181e
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-02-27 14:53:18 +0100

    ci: Print the result block of a leak test

--- .github/scripts/print_leak_test_cases.sh
+++ .github/scripts/print_leak_test_cases.sh
@@ -2,6 +2,7 @@
 
 TEST_OUTPUT=$(cat "$RESULT_FILE")
 LEAKS=0
+RESULT=0
 
 # Extract line numbers and file paths from test output
 while IFS= read -r line; do
@@ -18,6 +19,15 @@ while IFS= read -r line; do
     echo -e "\e[91m---------------------------------------------------------------------------------\e[1;97m"
     sed -n "${line_number},\$p" "$file_path" | awk 'NF {p=1} !NF {if(p) exit} {if(p) print}'
     echo -e "\e[0;91m=================================================================================\e[0m"
+  fi
+
+  # Check for the start of the result block and set the flag
+  if [[ $line == *"🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁"* ]]; then
+    RESULT=1
+  fi
+  # If the flag is set, print the line
+  if [[ $RESULT -ne 0 ]]; then
+    echo "$line"
   fi
 
   line_number=""
