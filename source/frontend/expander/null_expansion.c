8679da449da02f204a8b7fa543c5ac7ee9261d53
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-03-16 00:43:13 +0100

    chore: Reduce number of checks in null expansion check

--- source/frontend/expander/null_expansion.c
+++ source/frontend/expander/null_expansion.c
@@ -1,29 +1,36 @@
 #include "expander.h"
 
-static bool	is_null_expansion(char *str, t_list *task_list)
+static bool	is_null_expansion(char *str)
+{
+	if (!str || !*str)
+		return (true);
+	return (false);
+}
+
+static bool	any_quote_task(t_list *task_list)
 {
 	t_expander_task	*task;
 
-	if (str && *str)
-		return (false);
 	while (task_list)
 	{
 		task = task_list->content;
 		if (task->type == ET_QUOTE)
-			return (false);
+			return (true);
 		task_list = task_list->next;
 	}
-	return (true);
+	return (false);
 }
 
 void	check_null_expansions(t_list **lst, t_list *task_list)
 {
 	t_list	*cur;
 
+	if (any_quote_task(task_list))
+		return ;
 	cur = *lst;
 	while (cur)
 	{
-		if (is_null_expansion(cur->content, task_list))
+		if (is_null_expansion(cur->content))
 			ft_lstdrop_node(lst, &cur, free);
 		else
 			cur = cur->next;
