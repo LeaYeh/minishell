fc544dcbec2fb914677d608a51d1d66974824ecd
LeaYeh <smile2140@gmail.com>
2024-03-19 15:12:32 +0100

    fix: Fix norm issues of backend-redir module

--- source/backend/redirection/pipe_redirect.c
+++ source/backend/redirection/pipe_redirect.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipe_redirect.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 20:18:11 by lyeh              #+#    #+#             */
-/*   Updated: 2024/01/23 16:22:24 by codespace        ###   ########.fr       */
+/*   Updated: 2024/03/19 15:11:56 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,43 +45,4 @@ void	safe_close_all_pipes(t_shell *shell)
 {
 	safe_close_pipe(&shell->old_pipe);
 	safe_close_pipe(&shell->new_pipe);
-}
-
-// void	safe_move_nonempty_pipe(t_pipe *from, t_pipe *to)
-// {
-// 	if (*to->read_fd != -1 || *to->write_fd != -1)
-// 		printf("Warning: Pipe is not empty\n");
-// 	if (*from->read_fd != -1 && *to->read_fd != -1)
-// 	{
-// 		*to->read_fd = *from->read_fd;
-// 		*from->read_fd = -1;
-// 	}
-// 	if (*from->write_fd != -1 && *to->write_fd != -1)
-// 	{
-// 		*to->write_fd = *from->write_fd;
-// 		*from->write_fd = -1;
-// 	}
-// }
-
-bool	need_pipe(t_list_d *cmd_table_node)
-{
-	if (get_cmd_table_type_from_list(cmd_table_node) == C_SUBSHELL_START)
-		move_past_subshell(&cmd_table_node);
-	else if (get_cmd_table_type_from_list(cmd_table_node) == C_SIMPLE_CMD)
-		cmd_table_node = cmd_table_node->next;
-	if (get_cmd_table_type_from_list(cmd_table_node) == C_PIPE)
-		return (true);
-	return (false);
-}
-
-bool	create_pipe(t_pipe *new_pipe)
-{
-	if (new_pipe->pipe_fd[0] != -1 || new_pipe->pipe_fd[1] != -1)
-	{
-		printf("Warning: Pipe is not empty\n");
-		safe_close_pipe(new_pipe);
-	}
-	if (pipe(new_pipe->pipe_fd) == -1)
-		return (false);
-	return (true);
 }
