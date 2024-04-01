68f99024f2b6192d04ba166843cd54bb8ca01a01
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-04-01 18:36:34 +0200

    style: Improve function names in `ft_alphabetic()`

--- libraries/libft/src/libft/strings/ft_alphabetic.c
+++ libraries/libft/src/libft/strings/ft_alphabetic.c
@@ -6,15 +6,15 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 23:41:28 by ldulling          #+#    #+#             */
-/*   Updated: 2024/03/31 23:59:46 by ldulling         ###   ########.fr       */
+/*   Updated: 2024/04/01 18:35:35 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	compare_non_equal_alnum(const char *str1, const char *str2);
-static int	compare_equal_alnum(const char *str1, const char *str2);
-static int	compare_non_equal_non_alnum(const char *str1, const char *str2);
+static int	compare_case_insensitive(const char *str1, const char *str2);
+static int	compare_case_sensitive(const char *str1, const char *str2);
+static int	compare_non_alphanumeric(const char *str1, const char *str2);
 
 /**
  * The ft_alphabetic function compares two strings for the following order:
@@ -34,19 +34,19 @@ bool	ft_alphabetic(const char *str1, const char *str2)
 {
 	int	diff;
 
-	diff = compare_non_equal_alnum(str1, str2);
+	diff = compare_case_insensitive(str1, str2);
 	if (diff)
 		return (diff < 0);
-	diff = compare_equal_alnum(str1, str2);
+	diff = compare_case_sensitive(str1, str2);
 	if (diff)
 		return (diff < 0);
-	diff = compare_non_equal_non_alnum(str1, str2);
+	diff = compare_non_alphanumeric(str1, str2);
 	if (diff)
 		return (diff < 0);
 	return (ft_strcmp(str1, str2) <= 0);
 }
 
-static int	compare_non_equal_alnum(const char *str1, const char *str2)
+static int	compare_case_insensitive(const char *str1, const char *str2)
 {
 	while (*str1 && *str2)
 	{
@@ -65,7 +65,7 @@ static int	compare_non_equal_alnum(const char *str1, const char *str2)
 	return (*str1 - *str2);
 }
 
-static int	compare_equal_alnum(const char *str1, const char *str2)
+static int	compare_case_sensitive(const char *str1, const char *str2)
 {
 	while (*str1 && *str2)
 	{
@@ -84,7 +84,7 @@ static int	compare_equal_alnum(const char *str1, const char *str2)
 	return (*str1 - *str2);
 }
 
-static int	compare_non_equal_non_alnum(const char *str1, const char *str2)
+static int	compare_non_alphanumeric(const char *str1, const char *str2)
 {
 	while (*str1 && *str2)
 	{
