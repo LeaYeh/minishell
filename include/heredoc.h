ab1ad4d6bc1bcbfff743a78a987be7ba35bad871
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-04-07 19:19:00 +0200

    chore: Use heredoc enums and typedef

--- include/heredoc.h
+++ include/heredoc.h
@@ -15,9 +15,9 @@
 
 # include "defines.h"
 
-int		heredoc(t_sh *shell);
+t_hd_st	heredoc(t_sh *shell);
 bool	setup_tmp_hdfile(int cmdtable_id, t_io_red *io_red);
-int		expand_heredoc_content(t_sh *shell, char **content);
+t_hd_st	expand_heredoc_content(t_sh *shell, char **content);
 bool	remove_here_end_quote(t_sh *shell,
 			t_io_red *io_red, bool *need_content_expansion);
 bool	append_line_to_list(t_list **line_list, char *line);
