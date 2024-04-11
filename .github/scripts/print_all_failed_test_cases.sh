3399feaa20f54875a2eca9ee9e39144668bb4de2
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-04-11 14:29:20 +0200

    fix: Fix the output_file name search
    It was searching for a file ending with the line_number of the test case, but the file was created ending with the test_number of the test case.

--- .github/scripts/print_all_failed_test_cases.sh
+++ .github/scripts/print_all_failed_test_cases.sh
@@ -7,6 +7,7 @@ RESULT=0
 while IFS= read -r line; do
   if [[ $line == *"❌"* ]]; then
     echo "$line"
+    test_number=$(echo "$line" | grep -oP '\d+' | head -1 || true)
     line_number=$(echo "$line" | grep -oP '\d+' | tail -1 || true)
     file_path=$(echo "$line" | grep -oP '\s*'"$HOME"'/42_minishell_tester/cmds/.*\.sh' || true)
     file_basename=$(basename "${file_path%.*}")
@@ -16,14 +17,15 @@ while IFS= read -r line; do
   # Print test case
   if [[ -n $line_number && -n $file_path ]]; then
     echo -e "\e[0;94m---------------------------------------------------------------------------------\e[1;97m"
-    echo -e "\e[1;94mTEST CASE FROM $file_dirname/$file_basename AT LINE $line_number:\e[1;97m"
+    echo -e "\e[1;94mTEST CASE $test_number FROM $file_dirname/$file_basename AT LINE $line_number:\e[1;97m"
     sed -n "${line_number},\$p" "$file_path" | awk 'NF {p=1} !NF {if(p) exit} {if(p) print}'
     echo -e "\e[0;94m---------------------------------------------------------------------------------\e[1;97m"
 
     # Print stdout and stderr outputs
     for output_type in stdout stderr; do
       for shell_type in minishell bash; do
-        output_file="./tester_output/${file_dirname}/${file_basename}/${output_type}_${shell_type}_${line_number}"
+        output_file_name="${output_type}_${shell_type}_${test_number}"
+        output_file="./tester_output/${file_dirname}/${file_basename}/$output_file_name"
         if [[ -f $output_file ]]; then
           echo -e "\e[1;91m${output_type^^} OF ${shell_type^^}:\e[1;97m"
           cat "$output_file"
