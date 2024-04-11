79a11ca741e53fbc0ff2ba5e76745c388286f39b
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-04-11 11:39:26 +0200

    style: Improve readability of output in GH Actions

--- .github/scripts/print_all_failed_test_cases.sh
+++ .github/scripts/print_all_failed_test_cases.sh
@@ -16,22 +16,22 @@ while IFS= read -r line; do
   # Print test case
   if [[ -n $line_number && -n $file_path ]]; then
     echo -e "\e[0;94m---------------------------------------------------------------------------------\e[1;97m"
-    echo -e "\e[1;4;94mTEST CASE FROM $file_dirname/$file_basename AT LINE $line_number:\e[1;97m"
+    echo -e "\e[1;94mTEST CASE FROM $file_dirname/$file_basename AT LINE $line_number:\e[1;97m"
     sed -n "${line_number},\$p" "$file_path" | awk 'NF {p=1} !NF {if(p) exit} {if(p) print}'
-    echo -e "\e[0;91m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\e[1;97m"
+    echo -e "\e[0;94m---------------------------------------------------------------------------------\e[1;97m"
 
     # Print stdout and stderr outputs
     for output_type in std_out std_err; do
       for shell_type in minishell bash; do
         output_file="./tester_output/${file_dirname}/${file_basename}/${output_type}_${shell_type}_${line_number}"
         if [[ -f $output_file ]]; then
-          echo -e "\e[1;4;91m${output_type^^} OF ${shell_type^^}:\e[1;97m"
+          echo -e "\e[1;91m${output_type^^} OF ${shell_type^^}:\e[1;97m"
           cat "$output_file"
           echo -e "\e[0;91m---------------------------------------------------------------------------------\e[1;97m"
         fi
       done
     done
-    echo -e "\e[0;95m=================================================================================\e[0m"
+    echo -e "\e[1;95m=================================================================================\e[0m"
   fi
 
   # Check for the start of the result block and set the flag
