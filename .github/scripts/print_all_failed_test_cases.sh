e5b1af3188f55cd4561d3a9f55be031b8e1e83d4
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-04-11 18:10:15 +0200

    feat: Print diffs of outputs + differentiating colors

--- .github/scripts/print_all_failed_test_cases.sh
+++ .github/scripts/print_all_failed_test_cases.sh
@@ -12,27 +12,57 @@ while IFS= read -r line; do
     file_path=$(echo "$line" | grep -oP '\s*'"$HOME"'/42_minishell_tester/cmds/.*\.sh' || true)
     file_basename=$(basename "${file_path%.*}")
     file_dirname=$(basename $(dirname "$file_path"))
+    output_file_path="./tester_output/${file_dirname}/${file_basename}"
   fi
 
   # Print test case
   if [[ -n $line_number && -n $file_path ]]; then
-    echo -e "\e[0;94m---------------------------------------------------------------------------------\e[1;97m"
-    echo -e "\e[1;94mTEST CASE $test_number FROM $file_dirname/$file_basename AT LINE $line_number:\e[1;97m"
+    echo -e "\e[0;93m---------------------------------------------------------------------------------\e[1;97m"
+    echo -e "\e[1;93mTEST CASE $test_number FROM $file_dirname/$file_basename AT LINE $line_number:\e[1;97m"
     sed -n "${line_number},\$p" "$file_path" | awk 'NF {p=1} !NF {if(p) exit} {if(p) print}'
-    echo -e "\e[0;94m---------------------------------------------------------------------------------\e[1;97m"
+    echo -e "\e[0;93m---------------------------------------------------------------------------------\e[1;97m"
 
-    # Print stdout and stderr outputs
-    for output_type in stdout stderr; do
-      for shell_type in minishell bash; do
-        output_file_name="${output_type}_${shell_type}_${test_number}"
-        output_file="./tester_output/${file_dirname}/${file_basename}/$output_file_name"
-        if [[ -f $output_file ]]; then
-          echo -e "\e[1;91m${output_type^^} OF ${shell_type^^}:\e[1;97m"
-          cat "$output_file"
-          echo -e "\e[0;91m---------------------------------------------------------------------------------\e[1;97m"
-        fi
-      done
-    done
+    stdout_file_minishell="${output_file_path}/stdout_minishell_${test_number}"
+    stdout_file_bash="${output_file_path}/stdout_bash_${test_number}"
+    stderr_file_minishell="${output_file_path}/stderr_minishell_${test_number}"
+    stderr_file_bash="${output_file_path}/stderr_bash_${test_number}"
+
+    # Print diff of stdout between bash and minishell
+    if [[ -f $stdout_file_minishell && -f $stdout_file_bash ]]; then
+      echo -e "\e[1;36mDIFF OF \e[1;34mSTDOUT\e[1;36m BETWEEN BASH AND MINISHELL:\e[1;97m"
+      diff -u0 "$stdout_file_bash" "$stdout_file_minishell"
+      echo -e "\e[0;36m---------------------------------------------------------------------------------\e[1;97m"
+    fi
+    # Print stdout of minishell
+    if [[ -f $stdout_file_minishell ]]; then
+      echo -e "\e[1;91mSTDOUT OF MINISHELL:\e[1;97m"
+      cat "$stdout_file_minishell"
+      echo -e "\e[0;91m---------------------------------------------------------------------------------\e[1;97m"
+    fi
+    # Print stdout of bash
+    if [[ -f $stdout_file_bash ]]; then
+      echo -e "\e[1;92mSTDOUT OF BASH:\e[1;97m"
+      cat "$stdout_file_bash"
+      echo -e "\e[0;92m---------------------------------------------------------------------------------\e[1;97m"
+    fi
+    # Print diff of stderr between bash and minishell
+    if [[ -f $stderr_file_minishell && -f $stderr_file_bash ]]; then
+      echo -e "\e[1;36mDIFF OF \e[1;33mSTDERR\e[1;36m BETWEEN BASH AND MINISHELL:\e[1;97m"
+      diff -u0 "$stderr_file_bash" "$stderr_file_minishell"
+      echo -e "\e[0;36m---------------------------------------------------------------------------------\e[1;97m"
+    fi
+    # Print stderr of minishell
+    if [[ -f $stderr_file_minishell ]]; then
+      echo -e "\e[1;91mSTDERR OF MINISHELL:\e[1;97m"
+      cat "$stderr_file_minishell"
+      echo -e "\e[0;91m---------------------------------------------------------------------------------\e[1;97m"
+    fi
+    # Print stderr of bash
+    if [[ -f $stderr_file_bash ]]; then
+      echo -e "\e[1;92mSTDERR OF BASH:\e[1;97m"
+      cat "$stderr_file_bash"
+      echo -e "\e[0;92m---------------------------------------------------------------------------------\e[1;97m"
+    fi
     echo -e "\e[1;95m=================================================================================\e[0m"
   fi
 
