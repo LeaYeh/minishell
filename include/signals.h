362b8f45b64b1a10961c5a24c32aa78dc22b0478
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-06-01 11:29:39 +0200

    style: Remove prototype of non-existing function in signals.h

--- include/signals.h
+++ include/signals.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
+/*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 18:35:55 by lyeh              #+#    #+#             */
-/*   Updated: 2024/03/18 18:36:10 by lyeh             ###   ########.fr       */
+/*   Updated: 2024/05/21 23:10:49 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 
 void	handle_signal_std(int signo, siginfo_t *info, void *context);
 void	handle_signal_record(int signo, siginfo_t *info, void *context);
-void	handle_signal_heredoc(int signo, siginfo_t *info, void *context);
 void	setup_signal(t_sh *shell, int signo, t_sig state);
 void	raise_error_and_escape(t_sh *shell, char *msg);
 void	raise_error_to_all_subprocess(t_sh *shell, int exit_code, char *msg);
