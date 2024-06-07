0833e6aa708a817d3a403eab6bfcd63a7cc00122
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-06-07 07:36:18 +0200

    style: Make IO redirect names more clear

--- source/backend/redirection/stdio_bind.c
+++ source/backend/redirection/stdio_bind.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   stdio_bind.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
+/*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 15:09:45 by lyeh              #+#    #+#             */
-/*   Updated: 2024/05/03 17:18:22 by lyeh             ###   ########.fr       */
+/*   Updated: 2024/06/07 07:31:17 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,8 +57,7 @@ int	redirect_subshell_io(t_sh *shell, t_ct *cmd_table)
 
 	read_fd = -1;
 	write_fd = -1;
-	ret = handle_io_redirect(
-			shell, &read_fd, &write_fd, cmd_table->io_red_list);
+	ret = open_io_files(shell, &read_fd, &write_fd, cmd_table->io_red_list);
 	if (ret == SUCCESS && \
 		((read_fd != -1 && dup2(read_fd, STDIN_FILENO) == -1) || \
 		(write_fd != -1 && dup2(write_fd, STDOUT_FILENO) == -1)))
