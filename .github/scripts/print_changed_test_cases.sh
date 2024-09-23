221dc8665be0b44714675991c17d8bf86bc7ad00
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-09-23 21:43:33 +0200

    fix: Add the color fix also to Makefile, .sh and .yaml files

--- .github/scripts/print_changed_test_cases.sh
+++ .github/scripts/print_changed_test_cases.sh
@@ -5,9 +5,9 @@ diff_output=$(diff "$TARGET_LOG_FILE" "$SOURCE_LOG_FILE" || true)
 # Extract line numbers and file paths from diff output
 while IFS= read -r line; do
   if [[ $line == "<"* ]]; then
-    echo -e "\e[94m🎯 Target branch: $line\e[0m"
+    echo -e "\001\e[94m\002🎯 Target branch: $line\001\e[0m\002"
   elif [[ $line == ">"* ]]; then
-    echo -e "\e[93m🌱 Source branch: $line\e[0m"
+    echo -e "\001\e[93m\002🌱 Source branch: $line\001\e[0m\002"
     export LINE="$line"
     "$SCRIPTS_DIR/print_test_case.sh"
   fi
