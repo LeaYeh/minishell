4cb8e30f32c7f7acb8058039e243e40ba4e6ad65
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-09-24 23:42:11 +0200

    style: Use `\1`& `\2` instead of `\001` & `\002`
    Mentions of using the markers like this are also in the `man` of readline.

--- include/defines.h
+++ include/defines.h
@@ -56,16 +56,14 @@
 #  define PRETTY_PROG_NAME	"🌊rash"
 #  define PROMPT			"🌊rash$ "
 # else
-#  define PRETTY_PROG_NAME	\
-	"\001\e[3;34m\002🌊\001\e[0m\002\001\e[1;34m\002rash\001\e[0m\002"
-#  define PROMPT			\
-	"\001\e[3;34m\002🌊\001\e[0m\002\001\e[1;34m\002rash$\001\e[0m\002 "
+#  define PRETTY_PROG_NAME	"\1\e[3;34m\2🌊\1\e[0m\2\1\e[1;34m\2rash\1\e[0m\2"
+#  define PROMPT			"\1\e[3;34m\2🌊\1\e[0m\2\1\e[1;34m\2rash$\1\e[0m\2 "
 # endif
 
 # if NO_COLOR
 #  define HEREDOC_PROMPT	"> "
 # else
-#  define HEREDOC_PROMPT	"\001\e[1;37m\002> \001\e[0m\002"
+#  define HEREDOC_PROMPT	"\1\e[1;37m\2> \1\e[0m\2"
 # endif
 
 # define EXIT_MSG			"exit\n"
@@ -80,8 +78,7 @@
            Big thank you to @ValeriaGart for the pixel art!\n"
 # else
 #  define VALERIA_MSG		"\
-           Big thank you to \001\e[1m\002@ValeriaGart\001\e[0m\002 for the \
-pixel art!\n"
+           Big thank you to \1\e[1m\2@ValeriaGart\1\e[0m\2 for the pixel art!\n"
 # endif
 
 /* Error Codes */
@@ -124,29 +121,29 @@ pixel art!\n"
 #  define STY_GRA_BG		""
 #  define STY_WHI_BRI_BG	""
 # else
-#  define STY_RES			"\001\e[0m\002"
-#  define STY_BOL			"\001\e[1m\002"
-#  define STY_ITA			"\001\e[3m\002"
-#  define STY_UND			"\001\e[4m\002"
-#  define STY_RED			"\001\e[31m\002"
-#  define STY_GRE			"\001\e[32m\002"
-#  define STY_YEL			"\001\e[33m\002"
-#  define STY_BLU			"\001\e[34m\002"
-#  define STY_MAG			"\001\e[35m\002"
-#  define STY_CYA			"\001\e[36m\002"
-#  define STY_WHI			"\001\e[37m\002"
-#  define STY_GRA			"\001\e[90m\002"
-#  define STY_WHI_BRI		"\001\e[97m\002"
-#  define STY_BLA_BG		"\001\e[41m\002"
-#  define STY_RED_BG		"\001\e[41m\002"
-#  define STY_GRE_BG		"\001\e[42m\002"
-#  define STY_YEL_BG		"\001\e[43m\002"
-#  define STY_BLU_BG		"\001\e[44m\002"
-#  define STY_MAG_BG		"\001\e[45m\002"
-#  define STY_CYA_BG		"\001\e[46m\002"
-#  define STY_WHI_BG		"\001\e[47m\002"
-#  define STY_GRA_BG		"\001\e[100m\002"
-#  define STY_WHI_BRI_BG	"\001\e[107m\002"
+#  define STY_RES			"\1\e[0m\2"
+#  define STY_BOL			"\1\e[1m\2"
+#  define STY_ITA			"\1\e[3m\2"
+#  define STY_UND			"\1\e[4m\2"
+#  define STY_RED			"\1\e[31m\2"
+#  define STY_GRE			"\1\e[32m\2"
+#  define STY_YEL			"\1\e[33m\2"
+#  define STY_BLU			"\1\e[34m\2"
+#  define STY_MAG			"\1\e[35m\2"
+#  define STY_CYA			"\1\e[36m\2"
+#  define STY_WHI			"\1\e[37m\2"
+#  define STY_GRA			"\1\e[90m\2"
+#  define STY_WHI_BRI		"\1\e[97m\2"
+#  define STY_BLA_BG		"\1\e[41m\2"
+#  define STY_RED_BG		"\1\e[41m\2"
+#  define STY_GRE_BG		"\1\e[42m\2"
+#  define STY_YEL_BG		"\1\e[43m\2"
+#  define STY_BLU_BG		"\1\e[44m\2"
+#  define STY_MAG_BG		"\1\e[45m\2"
+#  define STY_CYA_BG		"\1\e[46m\2"
+#  define STY_WHI_BG		"\1\e[47m\2"
+#  define STY_GRA_BG		"\1\e[100m\2"
+#  define STY_WHI_BRI_BG	"\1\e[107m\2"
 # endif
 
 /* Symbols */
